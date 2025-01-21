4ac7f9c7c9a5eedd4829c09a4908b59f1ce26073
izabelle <personalife503@gmail.com>
2025-01-21 17:27:14 -0300

    style: "add hover"

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="md:mt-52 mt-16 text-center text-gray-300">
+    <div className="md:mt-52 mt-16 text-center text-gray-200 hover:text-gray-300">
       <a href="https://github.com/zabeluna">
         <h5>you're enough just as you are</h5>
         <h5> made with ♡ by luna</h5>
